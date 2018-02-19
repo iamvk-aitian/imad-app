a6643245bbf38a2cd7feda6f2decea3605e1fc28
vishal krishan <31375127+iamvk-aitian@users.noreply.github.com>
2018-02-19 14:17:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/main.js', function(req, res)   {
-    res.sendFile(path.join(_dirname, 'ui', 'main.js')); 
+    res.sendFile(path.join(__dirname, 'ui', 'main.js')); 
 });
 var names=[];
 app.get('/submit-name/:name', function(req, res){

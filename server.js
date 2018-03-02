7f9a33f057658fbfdea77d7ae52f6fd0831c5911
vishal krishan <31375127+iamvk-aitian@users.noreply.github.com>
2018-03-02 21:44:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,6 +115,10 @@ app.get('/check-login', function(req, res){
        res.send('You are not logged in');
    }
 });
+app.get('/logout', function(req, res){
+    delete req.session.auth;
+    res.send('You are logged out');
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));

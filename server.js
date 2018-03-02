e68aa11d12968525926d7048d19a40a66c61b6bc
vishal krishan <31375127+iamvk-aitian@users.noreply.github.com>
2018-03-02 15:28:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,6 +48,21 @@ app.get('/hash/:input', function(req, res){
     var hashedString = hash(req.params.input, 'it-will-gonna-be-awesome');
     res.send(hashedString);
 });
+app.get('/create-user', function(req, res){
+   //input-username, password
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password. salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES($1, $2)', [username, dbString], function(err, result){
+       if(err)
+        {
+			res.status(500).send(err.toString());
+		}
+		else
+        {
+			res.send('User Successfully created with username :' + username); 
+		}
+   });
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
